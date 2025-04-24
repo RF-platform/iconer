17ac3643ff8a3158bd56027f3bf88209b83f56eb
EronMG <eronmcgonagal@gmail.com>
2025-04-24 19:00:17 +0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Empty_Talic from "../public/assets/Empty_Talic.webp";
+import Empty_Talic from "/public/assets/Empty_Talic.webp";
 import {
   translations,
   itemTypeMap,
